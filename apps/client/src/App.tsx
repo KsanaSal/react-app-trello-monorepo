eb4531f5916d34f5d36a49b7f290ff59dc0ffe56
KsanaSal <oasalivon@gmail.com>
2024-03-30 20:58:13 +0200

    setting project

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    const [greeting, setGreeting] = useState("");
+
+    useEffect(() => {
+        fetch("/api").then((response) => {
+            response.text().then((text) => {
+                setGreeting(text);
+            });
+        });
+    }, []);
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>{greeting}</h1>
+        </>
+    );
+}
+
+export default App;
